5980210bc5755734dd4fdc223f5d21bca316c903
RodD35 <rtd13@students.uwf.edu>
2021-07-24 21:36:10 -0400

    Two-sum update

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,15 +1,18 @@
+/***
+ * @author Roderick Davis
+ * LeetCode #1 Two Sum
+ * return indices of the two numbers such that they add up to the target number
+ */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] sum = new int[2];
         for(int x = 0; x < nums.length; x++){
             for(int y = x + 1; y < nums.length; y++){
                 if((nums[x] + nums[y]) == target){
-                    sum[0] = x;
-                    sum[1] = y;
-                    return sum;
+                    return new int[] {x, y} ;
                 }
             }
         }
-        return sum;
+        return null;
     }
 }
